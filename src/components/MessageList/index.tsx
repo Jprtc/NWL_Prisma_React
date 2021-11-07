67235e0fc1e1cb81f62ca6258b9e9075ca170b8a
Joao Pedro de Rossi <jprtc8@gmail.com>
2021-11-06 22:59:56 -0300

    layout finished

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,46 @@
+import styles from './styles.module.scss'
+import logoImg from '../../assets/logo.svg'
+
+export function MessageList(){
+    return(
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021"/>
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptates perspiciatis iure voluptate,
+                         sed tempora totam facilis qui doloremque omnis. Corrupti dolor pariatur ut dolorum.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/jprtc.png" alt="João Pedro de Rossi"/>
+                        </div>
+                        <span>João Pedro de Rossi</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptates perspiciatis iure voluptate,
+                         sed tempora totam facilis qui doloremque omnis. Corrupti dolor pariatur ut dolorum.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/jprtc.png" alt="João Pedro de Rossi"/>
+                        </div>
+                        <span>João Pedro de Rossi</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptates perspiciatis iure voluptate,
+                         sed tempora totam facilis qui doloremque omnis. Corrupti dolor pariatur ut dolorum.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/jprtc.png" alt="João Pedro de Rossi"/>
+                        </div>
+                        <span>João Pedro de Rossi</span>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+    )
+}
