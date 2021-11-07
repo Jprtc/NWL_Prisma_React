e1f0a77484708a3293bfa79e4a9362fcdfc222b5
Joao Pedro de Rossi <jprtc8@gmail.com>
2021-11-07 00:39:34 -0300

    saving authentication token and last 3 messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,55 +1,47 @@
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
 import {api} from '../../services/api'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
+
+type Message={
+    id:string;
+    text:string;
+    user:{
+        name:string;
+        avatar_url:string;
+    }
+}
 
 export function MessageList(){
 
+    const [messages, setMessages] = useState<Message[]>([])
+
     useEffect(() =>{
-        api.get('messages/last3').then((response) =>{
-            console.log(response.data);
+        api.get<Message[]>('messages/last3').then((response) =>{
+            // console.log(response.data);
+            setMessages(response.data)
         })
-    }, [])
+    }, [messages])
 
     return(
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="DoWhile 2021"/>
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptates perspiciatis iure voluptate,
-                         sed tempora totam facilis qui doloremque omnis. Corrupti dolor pariatur ut dolorum.</p>
+                {messages.map(message => {
+                    return(
+                    <li key ={message.id} className={styles.message}>
+                     <p className={styles.messageContent}>{message.text}</p>
                     <div className={styles.messageUser}>
                         <div className={styles.userImage}>
-                            <img src="https://github.com/jprtc.png" alt="João Pedro de Rossi"/>
+                            <img src={message.user.avatar_url} alt={message.user.name}/>
                         </div>
-                        <span>João Pedro de Rossi</span>
+                        <span>{message.user.name}</span>
                     </div>
                 </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptates perspiciatis iure voluptate,
-                         sed tempora totam facilis qui doloremque omnis. Corrupti dolor pariatur ut dolorum.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/jprtc.png" alt="João Pedro de Rossi"/>
-                        </div>
-                        <span>João Pedro de Rossi</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptates perspiciatis iure voluptate,
-                         sed tempora totam facilis qui doloremque omnis. Corrupti dolor pariatur ut dolorum.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/jprtc.png" alt="João Pedro de Rossi"/>
-                        </div>
-                        <span>João Pedro de Rossi</span>
-                    </div>
-                </li>
-
+                    );
+                })}
             </ul>
         </div>
     )
