938ddec38785468706b037973aef941008cb9db6
Joao Pedro de Rossi <jprtc8@gmail.com>
2021-11-07 01:00:32 -0300

    authentication isolated

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,47 +1,15 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import {VscGithubInverted} from 'react-icons/vsc'
+import { AuthContext } from '../../context/auth';
 import { api } from '../../services/api';
 import styles from './styles.module.scss'
 
-type AuthResponse = {
-    token: string;
-    user:{
-        id: string;
-        avatar_url:string;
-        name: string;
-        login: string;
-    }
-}
 
-export function LoginBox(){
 
-    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=4dcad3537f6d4b6d3b98`;
+export function LoginBox(){   
+    const {signInUrl, user} = useContext(AuthContext)
 
-   async function signIn(githubCode: string){
-        const response = await api.post<AuthResponse>('authenticate',{
-            code: githubCode,
-        })
-
-        const {token,user} = response.data
-
-        localStorage.setItem('@dowhile:token', token)
-
-        console.log(user)
-    }
-
-    useEffect(() => {
-        const url = window.location.href;
-        const hasGithubCode = url.includes('?code=');
-
-        if(hasGithubCode){
-            const [urlWithoutCode, githubCode] = url.split('?code=')
-
-            window.history.pushState({},'', urlWithoutCode);
-            // console.log({urlWithoutCode, githubCode})
-
-            signIn(githubCode)
-        }
-    },[])
+    console.log(user)
 
     return(
         <div className={styles.loginBoxWrapper}>
